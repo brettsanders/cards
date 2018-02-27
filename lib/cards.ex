839b192e95428fcaef50964d40914274040a3c8b
Brett Sanders <brettrsanders@gmail.com>
2018-02-26 22:27:35 -0500

    Add docs for Cards.deal/2

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,6 +2,10 @@ defmodule Cards do
   @moduledoc """
     Provides methods for creating and handling a deck of cards
   """
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six",
       "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
@@ -31,14 +35,19 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and remainder of deck.
+    The `hand_size` argument indicates how many cards should be
+    in the hand.
 
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
-    # {hand, rest} returned by Enum.split is a Tuple
-    {hand, _} = deck
-    |> shuffle
-    |> Enum.split(hand_size)
-
-    hand
+    Enum.split(deck, hand_size)
   end
 
   def save(deck, filename) do
